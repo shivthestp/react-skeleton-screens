ee91b1c5fcf837c619a0fc2d92e7ae769c07ae61
Shiv Kumar <shivthestp@gmail.com>
2023-06-08 18:11:00 +0530

    Initial Project Setup with Grid Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Articles from './components/Articles';
+import Users from './components/Users';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>React Skeletons</h1>
       </header>
+      <div className='content'>
+        <Articles />
+        <Users />
+      
     </div>
+    </div>
+    
   );
 }
 
---
+++ src/components/Articles.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Articles() {
+  return (
+    <div className='article'>
+        <h2>Articles</h2>
+    </div>
+  )
+}
+
+export default Articles
